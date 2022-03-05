487978c650068f564426f19f4ad3ed4acba11037
Sofia M.A <sofia.ma@protonmail.com>
2022-03-06 04:09:52 +1300

    Reorderable relations

--- src/Beegraph.hs
+++ src/Beegraph.hs
@@ -26,29 +26,26 @@ module Beegraph
     -- todo
     (~>),
     vars,
-    queryByShape,
-    Leaper,
-    Jumper,
     Shaped (..),
-    equ,
-    saturate,
-    exists,
   )
 where
 
 import Control.Comonad (Comonad (extract))
 import Control.Comonad.Cofree (Cofree ((:<)))
 import Control.Lens hiding ((:<), (<.>))
-import Control.Monad.Free (Free (Free, Pure), MonadFree (wrap), iterA)
+import Control.Monad.Free (Free, MonadFree (wrap), iterA)
 import Control.Monad.Trans.Accum (AccumT, add, runAccumT)
 import Data.Foldable (maximum)
 import Data.Functor.Classes (Show1 (liftShowsPrec), showsBinaryWith, showsUnaryWith)
 import Data.IntMap qualified as IntMap
 import Data.IntSet qualified as IntSet
 import Data.Map qualified as Map
+import Data.Set qualified as Set
+import Data.Set.Lens (setOf)
 import Data.Traversable (for)
-import GHC.Exts qualified
+import GHC.Exts qualified as Ext
 import GHC.Show (Show (showsPrec))
+import Relation
 import Witherable (mapMaybe)
 import Prelude hiding (mapMaybe)
 
@@ -246,6 +243,12 @@ extractBee weigh = do
             update ((weigh (fmap (fst . extract) s), Id i) :< s) node (Id i)
 
 -- ──────────────────────────────── Queries ──────────────────────────────── --
+data Shaped f a = Shaped (f a) a
+  deriving (Show, Functor, Foldable, Traversable)
+
+termShape :: Lens' (Shaped f a) (f a)
+termShape = lens (\(Shaped f _) -> f) (\(Shaped _ k) g -> Shaped g k)
+
 submapBetween :: Ord k => (k, k) -> Map k a -> Map k a
 submapBetween (l, h) m = center
   where
@@ -253,184 +256,128 @@ submapBetween (l, h) m = center
     (eq, h', _greater) = Map.splitLookup h greaterEq
     center = maybe id (Map.insert l) l' $ maybe id (Map.insert h) h' eq
 
--- Int should be read to always be positive
-data Step
-  = Step
-  | Leap Int
+queryByShape :: forall f. Language f => f () -> BG f Nary
+queryByShape s = queryBetween (s $> minBound, s $> maxBound) (length s)
 
-data Leaper a
-  = End
-  | Continue Int a (Step -> Leaper a)
-  deriving (Functor)
-
-instance Foldable Leaper where
-  foldMap _ End = mempty
-  foldMap f (Continue _ a k) = f a <> foldMap f (k Step)
-
-instance Applicative Leaper where
-  pure a = go 0
-    where
-      go n = Continue n a \case
-        Step -> go (n + 1)
-        Leap i -> go i
-
-  End <*> _ = End
-  _ <*> End = End
-  Continue a b c <*> Continue d e f = case compare a d of
-    LT -> c (Leap d) <*> Continue d e f
-    EQ -> Continue a (b e) \step -> c step <*> Continue d e f
-    GT -> Continue a b c <*> f (Leap a)
-
-instance Alternative Leaper where
-  empty = End
-  End <|> a = a
-  a <|> End = a
-  Continue a b c <|> Continue d e f = case compare a d of
-    LT -> Continue a b \case
-      Step -> c Step <|> Continue d e f
-      Leap n -> c (Leap n) <|> f (Leap n)
-    EQ -> Continue a b \case
-      Step -> Continue d e f <|> c Step
-      Leap n -> c (Leap n) <|> f (Leap n)
-    GT -> Continue d e \case
-      Step -> Continue a b c <|> f Step
-      Leap n -> c (Leap n) <|> f (Leap n)
-
-instance IsList (Leaper a) where
-  type Item (Leaper a) = a
-  fromList :: [a] -> Leaper a
-  fromList = go 0
-    where
-      go _ [] = End
-      go n (s : ss) = Continue n s \case
-        Step -> go (n + 1) ss
-        Leap m -> go m $ drop (m - n) ss
-  toList = toList
-
-equ :: Leaper (Leaper ())
-equ = go 0
+queryBetween :: forall f. Language f => (f Id, f Id) -> Int -> BG f Nary
+queryBetween bounds limit = use share <&> go 0 . submapBetween bounds
   where
-    go n = Continue n (Continue n () (const End)) \case
-      Step -> go (n + 1)
-      Leap m -> go m
+    go :: Int -> Map (f Id) Id -> Nary
+    go depth _ | depth > limit = Never
+    go depth m =
+      let l = rel depth m
+       in Nary (fromList (map fst l)) (\i -> maybe Never (go (depth + 1)) (Map.fromList l ^? ix i))
 
-data Shaped f a = Shaped (f a) a
-  deriving (Show, Functor, Foldable, Traversable)
+    rel depth m =
+      Map.toAscList m
+        & mapMaybe
+          ( (\(a, b) -> (,b) <$> a)
+              . ( preview (traversed . index depth . unId)
+                    &&& \s ->
+                      submapBetween
+                        ( view termShape (s & traversed . indices (> depth) .~ minBound),
+                          view termShape (s & traversed . indices (> depth) .~ maxBound)
+                        )
+                        m
+                )
+              . uncurry Shaped
+          )
 
-termShape :: Lens' (Shaped f a) (f a)
-termShape = lens (\(Shaped f _) -> f) (\(Shaped _ k) g -> Shaped g k)
-
-type Jumper a = Free Leaper a
-
-queryByShape :: forall f a. Language f => ATraversal' (f Id) a -> f () -> BG f (Jumper (a, Id))
-queryByShape tr f = queryBetween (f $> Id 0, f $> Id maxBound) (length f) tr
-
-queryBetween :: forall f a. Language f => (f Id, f Id) -> Int -> ATraversal' (f Id) a -> BG f (Jumper (a, Id))
-queryBetween bounds depth tr = do
-  m <- submapBetween bounds <$> use share
-  pure $ go 0 depth m
-  where
-    tr' = cloneTraversal tr
-
-    go :: Int -> Int -> Map (f Id) Id -> Jumper (a, Id)
-    go depth limit m = fromMaybe (wrap End) do
-      (k, v) <- Map.lookupMin m
-      let s = Shaped k v
-          here :: (Indexable Int p, Traversable f1, Applicative f2) => p b (f2 b) -> f1 b -> f2 (f1 b)
-          here = traversed . index depth
-          (submap, rest) = Map.spanAntitone (\g -> (g ^? here) == (k ^? here)) m
-      i <- s ^? here
-      pure $
-        if depth + 1 == limit
-          then -- the wrap End case shouldn't happen for a valid Traversal'/shape combo
-            maybe (wrap End) (pure . (,v)) (k ^? tr')
-          else
-            wrap
-              ( Continue (_unId i) (go (depth + 1) limit submap) \case
-                  Step -> exists $ go depth limit rest
-                  Leap n -> exists $ go depth limit (Map.dropWhileAntitone (\y -> y ^? here . unId < Just n) rest)
-              )
-
-exists :: Jumper a -> Leaper (Jumper a)
-exists = \case
-  Pure _ -> End
-  Free le -> le
-
-triewalk :: Jumper a -> [a]
-triewalk (Pure a) = [a]
-triewalk (Free c) = concat $ go c
-  where
-    go End = []
-    go (Continue _ l f) = triewalk l : go (f Step)
+equ :: Nary
+equ = Nary [0 .. 8] (\i -> Nary [i] (const Never))
 
 newtype Var = Var {_unVar :: Word} deriving (Eq, Ord, Show)
 
 makeLenses ''Var
 
-vars :: [Var]
-vars = map Var [0 ..]
+data Stream a = a :& Stream a deriving (Functor)
+
+infixr 5 :&
+
+vars :: Stream Var
+vars = fmap Var (go 0)
+  where
+    go n = n :& go (n + 1)
 
 type Equ = (Var, Var)
 
+data Matcher f = Matcher
+  { _atoms :: [([Var], BG f Nary)]
+  }
+
+runMatcher :: Matcher f -> BG f Nary
+runMatcher (Matcher atoms) = do
+  atoms' <- traverse (\(vs, f) -> (vs,) <$> f) atoms
+  let vars' = setOf (folded . _1 . folded) atoms'
+  pure $ go vars' atoms'
+  where
+    go :: Set Var -> [([Var], Nary)] -> Nary
+    go vs xs = case Set.minView vs of
+      Nothing -> Never
+      Just (v, vs') ->
+        let rels = xs ^.. folded . filtered (\x -> (x ^? _1 . _head) == Just v) . _2 . _Nary . _1
+            joined = conjoin rels
+            update i =
+              xs <&> \(us, n) -> case us ^? _Cons of
+                Just (u, us')
+                  | u == v ->
+                    ( us',
+                      case n of
+                        Never -> Never
+                        Nary _ k -> k i
+                    )
+                _ -> (us, n)
+         in Nary joined (go vs' . update)
+
+matcher :: Language f => Free f Var -> (Matcher f, Var)
+matcher lhs =
+  let freshVar = Var $ maybe 0 (+ 1) $ maximumOf (traverse . unVar) lhs
+      (lhsHead, (equs, _freshVar', flattenedLHS)) = usingState (mempty :: Set Equ, freshVar, [] :: [Shaped f Var]) $ flip iterA lhs \f -> do
+        flatF <- sequence f
+        flatF' <- evaluatingStateT Nothing $ for flatF \v -> do
+          prev <- get
+          if
+              | Just u <- prev,
+                v <= u -> do
+                var' <- lift $ use _2
+                lift $ _2 . unVar += 1
+                lift (_1 . at (v, var') .= Just ())
+                put (Just var')
+                pure var'
+              | otherwise -> put (Just v) >> pure v
+        var <- use _2
+        _2 . unVar += 1
+        let sh = Shaped flatF' var
+        _3 %= cons sh
+        pure var
+      atoms = fmap (toList &&& queryByShape . void . view termShape) flattenedLHS ++ fmap (\(a, b) -> ([a, b], pure equ)) (toList equs)
+   in (Matcher atoms, lhsHead)
+
 data Rewrite f = Rewrite
-  { _lhs :: [Shaped f Var],
-    _lhsEqus :: Set (Var, Var),
+  { _lhs :: Matcher f,
     _lhsHead :: Var,
     _rhs :: Free f Var
   }
 
 (~>) :: Language f => Free f Var -> Free f Var -> Rewrite f
-(~>) lhs rhs =
-  let freshVar = Var $ maybe 0 (+ 1) $ maximumOf (traverse . unVar) lhs
-      (linearLHS, (_, equs, freshVar')) = usingState (mempty :: Set Var, mempty :: Set Equ, freshVar) $ for lhs \var -> do
-        b <- use (_1 . contains var)
-        if b
-          then do
-            fresh <- use _3
-            _2 . at (var, fresh) .= Just ()
-            _3 . unVar += 1
-            pure fresh
-          else do
-            _1 . at var .= Just ()
-            pure var
-      (lhsHead, (flattenedLHS, _freshVar'')) = usingState ([], freshVar') $ flip iterA linearLHS \f -> do
-        flatF <- sequence f
-        var <- use _2
-        _2 . unVar += 1
-        _1 %= cons (Shaped flatF var)
-        pure var
-   in Rewrite flattenedLHS equs lhsHead rhs
-
-joinList :: [Jumper a] -> Jumper [a]
-joinList = sequence
-
--- runRewrite :: Language f => Rewrite f -> BG f ()
--- runRewrite (Rewrite lhs equs head rhs) = do
---   _
-
-{-
-  (x, y, z) <-
-  F (G x) (H (F y) z)
-  -->
-  F a b, c; G x, a; H d z, b; F y, d;
-
- -}
+(~>) lhs rhs = Rewrite m h rhs where (m, h) = matcher lhs
 
 -- ────────────────────────── Equality saturation ────────────────────────── --
-saturate :: forall f. Language f => BG f (Jumper (Shaped f Id)) -> BG f ()
-saturate query = fixpoint (const go) ()
-  where
-    go :: AccumT Any (State (Beegraph f)) ()
-    go = do
-      j <- lift query
-      for_ j \(Shaped f i) -> do
-        new <- lift $ insertBee f
-        _ <- lift $ unionBee new i
-        add (Any True)
+-- saturate :: forall f. Language f => BG f (Jumper (Shaped f Id)) -> BG f ()
+-- saturate query = fixpoint (const go) ()
+--   where
+--     go :: AccumT Any (State (Beegraph f)) ()
+--     go = do
+--       j <- lift query
+--       for_ j \(Shaped f i) -> do
+--         new <- lift $ insertBee f
+--         _ <- lift $ unionBee new i
+--         add (Any True)
 
 -- ───────────────────────────── Test language ───────────────────────────── --
 data Foo a
   = H a a
+  | J a
   | G Int
   deriving (Eq, Ord, Show, Generic, Functor, Foldable, Traversable)
 
@@ -440,10 +387,11 @@ instance Language Foo
 
 instance Show1 Foo where
   liftShowsPrec sp sl p (H a b) = showsBinaryWith sp sp "H" p a b
+  liftShowsPrec sp sl p (J a) = showsUnaryWith sp "J" p a
   liftShowsPrec sp sl p (G i) = showsUnaryWith showsPrec "G" p i
 
 test :: String
-test = show $
+test = id $
   evaluatingState emptyBee $ do
     g0 <- insertBee (G 0)
     g1 <- insertBee (G 1)
@@ -454,8 +402,18 @@ test = show $
     f3 <- insertBee (H f2 g1)
     f4 <- insertBee (H f3 g1)
     f5 <- insertBee (H g3 g3)
+    j0 <- insertBee (J g0)
+    j1 <- insertBee (J g1)
+    j2 <- insertBee (J g2)
+    j3 <- insertBee (J j2)
     rebuild
-    habx <- queryByShape _H (H () ())
-    g0q <- queryByShape _G (G 3)
-    let s = wrap $ liftA2 const (exists habx) (exists g0q)
-    pure (triewalk s)
+    let (a :& b :& c :& _) = fmap pure vars
+    let (m, _) = matcher (wrap (H (wrap $ H a b) c))
+    nary <- runMatcher m
+    pure (debugTree nary)
+
+foo :: [[Word]]
+foo = fmap (fmap _unVar) $ map fst $ _atoms $ fst m
+  where
+    (a :& b :& c :& _) = fmap pure vars
+    m = matcher (wrap (H (wrap $ H a b) c))
--- src/Relation.hs
+++ src/Relation.hs
@@ -3,67 +3,94 @@
 
 module Relation where
 
-import Control.Lens hiding (Empty)
-import Data.Heap qualified as Heap
-import Data.Sequence (Seq (..))
-import GHC.Exts qualified as Ext
-import Text.Show qualified
-
-data RStep
-  = Step
-  | Leap Int
-
 data Rel
   = Null
-  | WithMin Int (RStep -> Rel)
+  | Nary (Map Int Rel)
 
-makePrisms ''Rel
 
-instance One Rel where
-  type OneItem Rel = Int
-  one i = WithMin i (const Null)
 
-instance IsList Rel where
-  type Item Rel = Int
+-- import Control.Lens hiding (Empty)
+-- import Data.Heap qualified as Heap
+-- import Data.Sequence (Seq (..))
+-- import GHC.Exts qualified as Ext
+-- import Text.Show qualified
 
-  fromList [] = Null
-  fromList (i : is) = WithMin i \case
-    Step -> fromList is
-    Leap n -> fromList (dropWhile (< n) is)
+-- -- data RStep
+-- --   = Step
+-- --   | Leap Int
 
-  toList Null = []
-  toList (WithMin i k) = i : Ext.toList (k Step)
+-- -- data Rel
+-- --   = Null
+-- --   | WithMin Int (RStep -> Rel)
 
-instance Show Rel where
-  show rel = show (Ext.toList rel)
+-- -- makePrisms ''Rel
 
-conjoin :: [Rel] -> Rel
-conjoin =
-  fmap (preview _WithMin) >>> sequence >>> maybe Null (fromList >>> go)
-  where
-    go :: Seq (Int, RStep -> Rel) -> Rel
-    go Empty = Null
-    go (rel :<| Empty) = review _WithMin rel
-    go (l :<| (mid :|> r)) =
-      let kSpin s = case s of
-            Null -> Null
-            WithMin a k -> go (mid :|> r :|> (a, k))
-       in if fst l == fst r
-            then WithMin (fst l) (kSpin . snd l)
-            else kSpin $ snd l (Leap (fst r))
+-- -- instance One Rel where
+-- --   type OneItem Rel = Int
+-- --   one i = WithMin i (const Null)
 
-disjoin :: [Rel] -> Rel
-disjoin =
-  fmap (preview _WithMin) >>> catMaybes >>> fmap (uncurry Heap.Entry) >>> Heap.fromList >>> go Nothing
-  where
-    go :: Maybe Int -> Heap.Heap (Heap.Entry Int (RStep -> Rel)) -> Rel
-    go m =
-      Heap.viewMin >>> \case
-        Nothing -> Null
-        Just (Heap.Entry i k, rest) ->
-          let f step = case k step of
-                Null -> go (m `max` Just i) rest
-                WithMin i' k' -> go (m `max` Just i) $ Heap.insert (Heap.Entry i' k') rest
-           in if Just i <= m
-                then f Step
-                else WithMin i f
+-- -- instance IsList Rel where
+-- --   type Item Rel = Int
+
+-- --   fromList [] = Null
+-- --   fromList (i : is) = WithMin i \case
+-- --     Step -> fromList is
+-- --     Leap n -> fromList (dropWhile (< n) is)
+
+-- --   toList Null = []
+-- --   toList (WithMin i k) = i : Ext.toList (k Step)
+
+-- -- instance Show Rel where
+-- --   show rel = "fromList " ++ show (Ext.toList rel)
+
+-- -- relMembers :: Fold Rel Int
+-- -- relMembers = folding Ext.toList
+
+-- -- conjoin :: [Rel] -> Rel
+-- -- conjoin =
+-- --   fmap (preview _WithMin) >>> sequence >>> maybe Null (sortOn fst >>> fromList >>> go)
+-- --   where
+-- --     go :: Seq (Int, RStep -> Rel) -> Rel
+-- --     go Empty = Null
+-- --     go (rel :<| Empty) = review _WithMin rel
+-- --     go (l :<| (mid :|> r)) =
+-- --       let kSpin s = case s of
+-- --             Null -> Null
+-- --             WithMin a k -> go (mid :|> r :|> (a, k))
+-- --        in if fst l == fst r
+-- --             then WithMin (fst l) (kSpin . snd l)
+-- --             else kSpin $ snd l (Leap (fst r))
+
+-- -- disjoin :: [Rel] -> Rel
+-- -- disjoin =
+-- --   fmap (preview _WithMin) >>> catMaybes >>> fmap (uncurry Heap.Entry) >>> Heap.fromList >>> go Nothing
+-- --   where
+-- --     go :: Maybe Int -> Heap.Heap (Heap.Entry Int (RStep -> Rel)) -> Rel
+-- --     go m =
+-- --       Heap.viewMin >>> \case
+-- --         Nothing -> Null
+-- --         Just (Heap.Entry i k, rest) ->
+-- --           let f step = case k step of
+-- --                 Null -> go (m `max` Just i) rest
+-- --                 WithMin i' k' -> go (m `max` Just i) $ Heap.insert (Heap.Entry i' k') rest
+-- --            in if Just i <= m
+-- --                 then f Step
+-- --                 else WithMin i f
+
+-- -- data Nary
+-- --   = Never
+-- --   | Nary Rel (Int -> Nary)
+
+-- -- makePrisms ''Nary
+
+-- -- debugTree :: Nary -> String
+-- -- debugTree = toString . unlines . fmap (\(i, t) -> toText (replicate i ' ') <> t) . go (-1)
+-- --   where
+-- --     go i Never = [(i, "Never")]
+-- --     go i (Nary rel k) = concatMap (\j -> (i + 1, show j <> ":") : go (i + 2) (k j)) (Ext.toList rel)
+
+-- -- depthy :: Nary -> String
+-- -- depthy = show . go
+-- --   where
+-- --     go Never = 0
+-- --     go (Nary rel k) = 1 + go (k 0)
